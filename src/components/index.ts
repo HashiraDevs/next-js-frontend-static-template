bfa6f942609974f033159ee40a4521ffa914a976
Karume-lab <danielkarume@outlook.com>
2024-10-15 19:09:19 +0300

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import Footer from './Footer';
+import Header from './Header';
+import HeroSection from './HeroSection';
+import SiteLogo from './SiteLogo';
+
+
+
+export {
+    Footer,
+    Header,
+    HeroSection,
+    SiteLogo,
+}
